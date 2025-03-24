477eb00b4604893c4d955b668d15409e974a7448
lokesh <21be0158@sipnaengg.ac.in>
2025-03-24 17:00:45 +0530

    Add cors

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cookieParser = require("cookie-parser");
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://placement-portal-seven.vercel.app/"],
     method: ["POST", "GET"],
     credentials: true,
   })
